f1adff3a28240eb42eda6ad99de648fda3e6e436
aktan <76080041+raiym2048@users.noreply.github.com>
2024-03-28 16:34:36 +0600

    Initial commit

---
+++ src/main/java/kg/it_lab/backend/Flow/FlowApplication.java
@@ -0,0 +1,13 @@
+package kg.it_lab.backend.Flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/it_lab/backend/Flow/FlowApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.it_lab.backend.Flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
